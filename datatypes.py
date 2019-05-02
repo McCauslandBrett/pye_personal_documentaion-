388119ee9f829b0e6de9f9c332286b65effe815b
Brett <brett@Bretts-MacBook-Pro.local>
2019-05-02 12:37:31 -0700

    template

---
+++ datatypes.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Documentation By Brett
+"""
+
+#list 
+def lst_driver():
+    lst = []
+   
+    
+#dictionaries    
+def dct_driver():
+    dct = {}
+  
+    
+#tuples  
+def tup_driver():
+    tup = {'a',1}
+    #insertion
+ 
+    
+
+
+
+"""
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+comments: 
+    
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"""
+#list
+    
+# list creation
+lst = []
+#list insertion
+    
+# list deletion
+    
+# list mutators
+    
+# list copy
+
+# list operators
+
+"""
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+comments: 
+    
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"""
+#dictionary
+
+# dictionary creation
+dct = {}
+
+# dictionry insertion
+
+# dictionary deletion
+
+# dictionary mutators
+
+# dictioary copy
+
+# dictionary sorted
+
+"""
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+comments: 
+    
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+"""
+#tuples
+
+# tuples creation
+tup = {'a',1}
+# tuples insertion
+# tuples deletion
+# tuples mutators
+# tuples copy
+# tuples sorted
+
+
---
+++ panda.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: brett
+Panda is a Library that provides data structures and data analysis tools
+"""
+import pandas as pd
+
+
+def df_driver():
+    df_expl= pd.DataFrame()
+
+   
+
+
+"""
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+comments: 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"""
+#dataframe
+    
+# dataframes creation
+    #-- Creating a empty dataframe 
+df = pd.DataFrame()
+ 
+#dataframe insertion
+    
+# dataframe deletion
+    
+# dataframe mutators
+    
+# dataframe copy
+
+# dataframe operators
+"""
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+comments: 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"""
